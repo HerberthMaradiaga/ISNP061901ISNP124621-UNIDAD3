83b23590a6437d78dae1da04782ac7bfd41ec676
mined <mined@DESKTOP-6DCQLV5>
2022-12-04 21:28:22 -0600

    codificacion clase auto

--- Form2.vb
+++ Form2.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form2
+    Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
 
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+
+    End Sub
 End Class
